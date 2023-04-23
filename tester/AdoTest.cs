c1175ca9a3e0fa016f5260a2e21be6e52f520aeb
Shakhawat Hossain <shakhawathossain.se@gmail.com>
2023-04-23 13:16:46 +0600

    nothing to commit

---
+++ tester/AdoTest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+
+
+
+namespace tester
+{
+    public class AdoTest
+    {
+
+        private string _connectionString;
+
+        public AdoTest(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public void ExecuteNonQuery(string commandText, SqlParameter[] parameters = null)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand command = new SqlCommand(commandText, connection))
+                {
+                    if (parameters != null)
+                    {
+                        command.Parameters.AddRange(parameters);
+                    }
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public DataTable ExecuteQuery(string commandText, SqlParameter[] parameters = null)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand command = new SqlCommand(commandText, connection))
+                {
+                    if (parameters != null)
+                    {
+                        command.Parameters.AddRange(parameters);
+                    }
+
+                    connection.Open();
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                    {
+                        DataTable result = new DataTable();
+                        adapter.Fill(result);
+                        return result;
+                    }
+                }
+            }
+
+
+        }
+    }
+}
--- tester/Program.cs
+++ tester/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine( "df" );
+
+using System.Data.SqlClient;
+using tester;
+
+string connectionString = "Server=DESKTOP-8VMMQPN\\SQLEXPRESS;Database=bookzone;Trusted_Connection=True;Encrypt=False";
+AdoTest db = new AdoTest(connectionString);
+
+string commandText = "INSERT INTO Authors(Name, Address, ImgDir) VALUES (@Value1, @Value2, @Value3)";
+SqlParameter[] parameters = new SqlParameter[]
+{
+    new SqlParameter("@Value1", "Hello"),
+    new SqlParameter("@Value2", "World"),
+        new SqlParameter("@Value3", "World")
+};
+
+db.ExecuteNonQuery(commandText, parameters);
